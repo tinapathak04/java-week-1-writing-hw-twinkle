e584d70eddbef01828d759cb1d2e9a14ef151afd
tinapathak04 <tinapathak040@gmail.com>
2023-05-18 12:47:21 +0100

    submitted

---
+++ src/homework/Programme_2_PrintFace.java
@@ -0,0 +1,15 @@
+package homework;
+/**
+ * 2. Write a Java program to print a face.
+ */
+public class Programme_2_PrintFace {
+
+        public static void main(String[] args) {
+            System.out.println(" +\"\"\"\"\"+");
+            System.out.println("[| o o |]");
+            System.out.println(" |  ^  | ");
+            System.out.println(" | '_' | ");
+            System.out.println(" +_____+");
+        }
+    }
+
